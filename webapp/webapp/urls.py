d0eb986f47bd5798af8f76b6c3993e5e54e5afcf
waliaree <freelancerneverbet@gmail.com>
2022-09-04 10:50:20 +0500

    commit on

--- webapp/webapp/urls.py
+++ webapp/webapp/urls.py
@@ -21,6 +21,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('login.urls'))
-     path('signin/', admin.site.urls),
+    path('signin/', admin.site.urls),
 ]
 
